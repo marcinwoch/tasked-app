c9b22906110c5d1520a5001716f954e73e459a31
PLMWAR <marcin.woch@sweco.pl>
2022-05-10 09:26:18 +0200

    Create list of object(todo item)

--- src/app-refactor.js
+++ src/app-refactor.js
@@ -1,45 +1,82 @@
-function createBlueprint () {
+function createBlueprint() {
     const todoRootElement = document.createElement('div');
     todoRootElement.setAttribute("class", "todo-item");
-  
+
     const checkboxElement = document.createElement('input');
     checkboxElement.setAttribute("type", "checkbox");
     checkboxElement.setAttribute("class", "todo-check")
-  
+
     const textareaElement = document.createElement('textarea');
     textareaElement.setAttribute("type", "text");
     textareaElement.setAttribute("class", "todo-text");
     textareaElement.setAttribute("rows", "1");
     textareaElement.setAttribute("placeholder", "Add task");
-  
+
     const buttonElement = document.createElement('button');
     buttonElement.setAttribute("class", "erase-task-btn");
-  
+
     const eraseElement = document.createElement('i');
     eraseElement.setAttribute("class", "fa fa-times");
     eraseElement.setAttribute("aria-hidden", "true");
-  
+
     todoRootElement.appendChild(checkboxElement);
     todoRootElement.appendChild(textareaElement);
     todoRootElement.appendChild(buttonElement);
-    buttonElement.appendChild(eraseElement); 
-     
+    buttonElement.appendChild(eraseElement);
+
     return todoRootElement;
 }
 
-let taskList = [];
-
-const taskElement = {
-    id: "ID#" + Math.floor(Math.random() * 100000),
-    blueprint: createBlueprint(),
-    isDone: false
+function createID() {
+    let newID = "ID#" + Math.floor(Math.random() * 10000);
+    return newID;
 }
 
-taskList.push(taskElement);
-taskList.push(taskElement);
+function handleBtn() {
+    const btn = document.querySelector("#add-item-btn");
+    btn.addEventListener("click", function () {
+        let taskContainer = document.querySelector("#task-container");
+        let createNewID = createID();
+        //   console.log(createNewID);
+        let newItemElement = Object.assign({}, itemElement);
+        newItemElement.id = String(createNewID);
+        //   console.log(newItemElement);
+        taskList.push(newItemElement);
+        console.log(taskList);
+        // console.log(taskList.length);
 
-console.log(taskList);
-console.log(taskElement.blueprint);
+        for (let i = 0; i < taskList.length; i++) {
+            console.log(taskList[i].content);
+            taskContainer.prepend(taskList[i].content);
+        }
+
+
+        // taskList.forEach(function (index) {
+        //     let list = 
+        //     console.log(index.content);
+
+        // })
+
+    });
+}
+
+
+let taskList = [];
+
+const blueprint = createBlueprint();
+
+let itemElement = {
+    id: "",
+    content: blueprint,
+    isDone: false
+};
+
+handleBtn();
+
+  // https://jsbin.com/minutesoye/edit?html,js,console,output
+
+
+
 
 
 
